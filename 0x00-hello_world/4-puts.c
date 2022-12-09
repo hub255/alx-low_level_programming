566aae812ae5c3a7c152892da3fe843659abd24e
Bobby <otienoagd@gmail.com>
2022-12-08 21:34:40 -0800

    put ch

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+
+ * main - Entry point
+
+ *
+
+ *Description: Refers to main
+
+ *Return: Always 0 (Success)
+
+ */
+
+int main(void)
+  
+{
+  
+  puts("\"Programming is like building a multilingual puzzle");
+  
+  return (0);
+  
+}
