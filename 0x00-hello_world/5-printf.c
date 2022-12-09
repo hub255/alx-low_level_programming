0072aa62b964b01d2cc1fc22f100841aa6d5d19f
Bobby <otienoagd@gmail.com>
2022-12-08 21:58:44 -0800

    print no2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,8 @@
 *Description: Refers t main
 *Return: Always 0 (Success)
 */
-int main(void)  
-{  
-printf("with proper grammar, but the outcome is a piece of art,\n"); 
-return (0); 
+int main(void) 
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
