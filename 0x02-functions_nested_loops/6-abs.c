9225c4547bb23cc2fae55c3475934cc1ca50a0e5
Bobby <otienoagd@gmail.com>
2022-12-14 01:04:21 -0800

    abs of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_abs- prints absolute number
+*
+*@i: character to use
+*Return: a positive integer
+*/
+int _abs(int i)
+{
+if (i > 0)
+return (i);
+else
+return (i * -1);
+}
