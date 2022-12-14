e4d40b66cc4dc17405f9c7f1636fdae460f30173
Bobby <otienoagd@gmail.com>
2022-12-14 00:12:16 -0800

    put char to stdout

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,18 @@
-#include "holberton.h"
+#include "main.h"
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
+*main-print putchar
+*Return: Always 0
 */
 int main(void)
 {
-char *sh = "Holberton";
-while (*sh)
-{
-_putchar(*sh);
-sh++;
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
