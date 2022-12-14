397f52302ece338c5cf4fbef93c76443c5fb3220
Bobby <otienoagd@gmail.com>
2022-12-14 00:35:50 -0800

    compare char for lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_islower - prints lower case
+*
+*@c: character to compare
+*Return: 1 if True, 0 if false
+*/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+return (0);
+}
