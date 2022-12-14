7f70146f7b80c560fec9992d5d8f22743ef5cf1f
Bobby <otienoagd@gmail.com>
2022-12-13 23:27:36 -0800

    put char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,7 @@
 */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+printf("_putchar");
+printf("\n");
+return 0;
 }
