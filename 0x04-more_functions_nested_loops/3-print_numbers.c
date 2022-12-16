6d1006a8b4df003f06c4c77d484aa2b1537374dd
Bobby <otienoagd@gmail.com>
2022-12-16 09:07:29 -0800

    print nums from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_numbers - Print the numbers since 0 up to 9
+*
+* Return: The numbers since 0 up to 9
+*/
+void print_numbers(void)
+{
+int y;
+for (y = 0; y <= 9; y++)
+{
+_putchar(y + '0');
+}
+_putchar('\n');
+}
