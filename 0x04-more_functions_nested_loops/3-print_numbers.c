64073f7c8fc39f08bedc8222d36be577fd4a1a21
Bobby <otienoagd@gmail.com>
2022-12-16 09:24:18 -0800

    print nums

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,11 +2,12 @@
 /**
 * print_numbers - prints the numbers
 *
+* Return - from 0-9
 */
 void print_numbers(void)
 {
 int i;
-for (i = '0'; i <= '9'; i++)
+for (i = 0; i <= 9; i++)
 {
 _putchar(i);
 }
