920a5182737158c593a9394869a47035be371812
Bobby <otienoagd@gmail.com>
2023-01-05 09:02:40 -0800

    put char

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,30 +1,17 @@
 #include "main.h"
-
 #include "6-abs.c"
-
 #include <stdio.h>
-
 /**
-
- *print_to_98 - print numbers
-
- *
-
- *@j: compare with j
-
- *Return: number followed by space
-
- */
-
+*print_to_98 - print numbers
+*
+*@j: compare with j
+*Return: number followed by space
+*/
 void print_to_98(int j)
-  
 {
-  
-  int i;
-  
-  if (j > 98)
-    
-    {
+int i;
+if (j > 98)
+{
       
       for (i = j; i >= 98; i--)
 	
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-low level programming
+recursion
