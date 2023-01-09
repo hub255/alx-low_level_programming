3d2e23f9f61afd5a31c482376040edae54c5e64b
Bobby <otienoagd@gmail.com>
2023-01-09 09:13:31 -0800

    multiply 2 nos

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,12 +10,12 @@
 */
 int main(int argc, char *argv[])
 {
-int n1 = 0, n2 = 0;
+int c = 0, s = 0;
 if (argc == 3)
 {
-n1 = atoi(argv[1]);
-n2 = atoi(argv[2]);
-printf("%d\n", n1 * n2);
+c = atoi(argv[1]);
+s = atoi(argv[2]);
+printf("%d\n", c * s);
 }
 else
 {
