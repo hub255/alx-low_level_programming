b13f575a2e668495f6546c626fda5a5c12fb588c
Bobby <otienoagd@gmail.com>
2023-01-11 09:29:04 -0800

    cncat

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,3 +2,4 @@ char *create_array(unsigned int size, char c);
 void free_grid(int **grid, int height);
 int **alloc_grid(int width, int height);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
